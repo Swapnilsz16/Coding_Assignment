092cc964a7e9c17953430169e20185d1baf36d91
Swapnilsz16 <113876203+Swapnilsz16@users.noreply.github.com>
2023-02-07 18:51:38 +0530

    Delete Springboot_Backend directory

--- Springboot_Backend/User_Managment_System/src/main/java/com/ums/repositories/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.ums.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ums.entities.User;
-
-public interface UserRepo extends JpaRepository<User, Integer> {
-
-}
